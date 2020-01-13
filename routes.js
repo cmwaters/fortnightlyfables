b5dee2095b64e14978eed1d8be24c85e3458bf02
Callum Michael Waters <c.waters@campus.tu-berlin.de>
2020-01-13 23:54:18 +0100

    created draft static landing page

--- routes.js
+++ routes.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-router.get('/story/alpha', function(req, res) {
-  res.render('alpha/draft')
+router.get('/story/a_drop_of_sense_in_a_river_of_madness', function(req, res) {
+  res.render('a_drop_of_sense_in_a_river_of_madness/first_publish')
 });
 
 module.exports = router;
