d24906ec159198aa6eabf8c555d16e62dcced9fb
Callum Michael Waters <c.waters@campus.tu-berlin.de>
2020-02-10 18:37:20 +0100

    used images instead of titles. small modifications to layout

--- routes.js
+++ routes.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('library');
 });
 
-router.get('/story', function(req, res, next) {
+router.get('/story', function(req, res) {
   res.render('library');
 });
 
